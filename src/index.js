b7d0ce4a443750e6a399f53eaa46692f0bb83e24
Gergo Nagy <guzmop@gmail.com>
2017-11-22 11:52:00 +0000

    npm stuff installed and set up the file stru

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
+import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
+import App from './components/app'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route path='/' component={App} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.querySelector('.container'));
+
+
 registerServiceWorker();

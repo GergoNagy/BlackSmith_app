6090efe3a058243e3d8ff16c71b453901831cf96
Gergo Nagy <guzmop@gmail.com>
2017-12-05 12:32:06 +0000

    add the slide show to the welcome page

--- src/components/slide_show.js
+++ src/components/slide_show.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import BackgroundSlideshow from 'react-background-slideshow'
+import image1 from './pic/level.jpg'
+import image2 from './pic/bopen.jpg'
+import image3 from './pic/mh2.jpg'
 
 export default class SlideShow extends Component {
+    
+
+
     render(){
         return(
             <div>
-                Pic
-                <img className='img' alt='loading' src={require("./pic/level.jpg")}/>
+                <BackgroundSlideshow images={[image1, image2, image3]}/>
             </div>
         )
     }

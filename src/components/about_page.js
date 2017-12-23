2cabf7fcc5713bd2b75f7cb1687d841a8e62e538
Gergo Nagy <guzmop@gmail.com>
2017-12-23 13:53:44 +0000

    added the validation and some css for the validation, submit button and on submit function

--- src/components/about_page.js
+++ src/components/about_page.js
@@ -4,25 +4,39 @@ import NavBar from './nav_bar'
 
 class AboutPage extends Component{
     renderField(field){
+        const { meta: { touched, error } } = field
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`
+
         return (
-            <div className='form-group'>
+            <div className={className}>
                 <label>{field.label}</label>
                 <input
                     className='form-control'
                     type='text'
                     {...field.input}
                 />
+                <div className='text-help'>
+                    {touched ? error : ''}
+                </div>
             </div>
         )
     }
 
+    onSubmit(values) {
+        console.log(values)
+    }
+
     render(){
+        const { handleSubmit } = this.props
+        
         return(
             <div>
                 <NavBar />
                 About Page(this will be the place to talk about the company)
 
-                <form>
+                <h3>Send me a message:</h3>
+
+                <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                     <Field 
                         name='name'
                         label='Name'
@@ -34,16 +48,37 @@ class AboutPage extends Component{
                         component={this.renderField}
                     />
                     <Field 
+                        name='phoneNumber'
+                        label='Phone Number'
+                        component={this.renderField}
+                    />
+                    <Field 
                         name='message'
                         label='Message'
                         component={this.renderField}
                     />
+                    <button type='submit' className='btn btn-primary'>Send</button>
                 </form>
             </div>
         )
     } 
 }
 
+function validate(values) {
+    const errors = {}
+    if (!values.name) {
+        errors.name = 'Enter a title!'
+    }
+
+    if(!values.email) {
+        errors.email = 'Enter your email adress!'
+    }
+
+
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'Messages'
 })(AboutPage)
